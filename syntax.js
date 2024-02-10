eb19be089b71a79c265cba634beeca2d9124062e
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-10 15:03:33 +0000

    JavaScript Syntax

---
+++ syntax.js
@@ -0,0 +1,16 @@
+const firsName = "Nunthachai";
+const lastName = "Yuennan"
+
+const val = 42
+
+const arr = [
+    'string',
+    42,
+    function() { console.log('h1') },
+]
+
+arr[2]()
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
